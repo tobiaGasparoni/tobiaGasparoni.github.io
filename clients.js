995558c7270f5096b450678380f461fea147baf5
TOBIA RAFFAELE GASPARONI BERDUGO <tr.gasparoni@sis.virtual.uniandes.edu.co>
2018-10-24 16:10:17 -0500

    Version de la U ingresada

---
+++ clients.js
@@ -0,0 +1,38 @@
+var myClients = {
+  "clients": [
+    {
+      "name": "Tobia Gasparoni",
+      "email": "tobia.gasparoni@gmail.com",
+      "projects": [
+        {
+          "name": "Restaurante italiano",
+          "status": "done",
+          "lastModified": "2015-01-02T05:00:00.000Z"
+        },
+        {
+          "name": "Patinaje sobre hielo",
+          "status": "in progress",
+          "lastModified": "2018-01-02T05:00:00.000Z"
+        }
+      ]
+    },
+    {
+      "name": "Camilo Gutiérrez",
+      "email": "camilo.gutiérrez@gmail.com",
+      "projects": [
+        {
+          "name": "Spotify colombiano",
+          "status": "done",
+          "lastModified": "2013-01-02T05:00:00.000Z"
+        },
+        {
+          "name": "Patinaje sobre hielo",
+          "status": "in progress",
+          "lastModified": "2019-01-02T05:00:00.000Z"
+        }
+      ]
+    }
+  ]
+}
+
+console.log(myClients);
--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-function blog()
-{
-	
-}
-
-function login()
-{
-	var cmd = document.getElementById("cmd");
-
-	var info = document.createElement("input");
-	info.setAttribute("value", "Insert username below and press ENTER");
-	info.setAttribute("readonly", true);
-	cmd.appendChild(info);
-
-	var input = document.createElement("input");
-	input.setAttribute("id", "input");
-	cmd.appendChild(input);
-
-	var inputEvent = document.getElementById("input");
-	inputEvent.addEventListener("keydown", function(e)
-	{
-	    if (e.keyCode === 13)
-	    {
-	    	validate(inputEvent.value);
-	    }
-	});
-}
-
-function validate(username)
-{
-
-    alert('Username is ' + username);
-}
+var cmd = document.getElementById("cmd");
 
 function admin()
 {
+	document.getElementById("cmd").value += ("\nGato favorito");
 	
+	var inputEvent = document.getElementById("input");
+	inputEvent.addEventListener("keydown", gato(e));
+}
+
+function gato(e)
+{
+	if (e.keyCode === 13)
+	    {
+	    	if(document.getElementById("input").value === "Basilio")
+			{
+				document.getElementById("cmd").value += ("\nGato favorito aprobado: Basilio");
+				document.getElementById("cmd").value += ("\nIngrese contraseña");
+				inputEvent.addEventListener("keydown", password(e));
+			}
+			else
+				document.getElementById("cmd").value += ("\nAcceso denegado: gato favorito no es " + document.getElementById("input").value);
+			document.getElementById("input").value = "";
+	    }
+}
+
+function password(e)
+{
+	if (e.keyCode === 13)
+	{
+		if(document.getElementById("input").value === "Contraseña")
+		{
+			document.getElementById("cmd").value += ("\nAcceso aprobado");
+			document.getElementById("cmd").value += ("\nAquí están sus proyectos");
+		}
+		else
+			document.getElementById("cmd").value += ("\nAcceso denegado: contraseña incorrecta");
+		document.getElementById("input").value = "";
+	}
 }
